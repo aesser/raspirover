6bac6a03340f742665469190b8127380e1bf3e06
Vinzenz Gregor Eck <vinzenz@xal.no>
2018-10-23 12:17:18 +0200

    Outlined project functions

--- main.py
+++ main.py
@@ -2,19 +2,17 @@ import time
 import logging
 from rrb3 import *
 
-rr = RRB3()
 
-led1 = 0
-led2 = 1
-
-canRun = True
+def calibrate_compass(rr):
+    """function to calibrate compass"""
+    pass
 
 
-logger = logging.getLogger()
-counter = 0
-while canRun:
-    try:
-
+def blink_lights(rr, n_blinks, sleep_time=0.5):
+    """show blinking lights"""
+    led1 = 0
+    led2 = 1
+    for i in range(n_blinks):
         rr.set_led1(led1)
         rr.set_led2(led2)
 
@@ -22,15 +20,35 @@ while canRun:
         led2 = led1
         led1 = tmp
 
-        time.sleep(0.5)
+        time.sleep(sleep_time)
 
-        counter += 1
-        if counter > 20:
-            break
 
-        logger.info("switch")
-    except RuntimeError:
-        logger.error("Runtime Error")
+def measure_distance():
+    """measure distance and average"""
+    pass
 
-logger.info("Bye")
 
+def go_direction():
+    """functioni to run the robot a ceratin direction"""
+    pass
+
+
+def turn():
+    """functio to turn the robot"""
+    pass
+
+
+def main_loop():
+    """The main loop controlling the robot"""
+    rr = RRB3()
+
+    calibrate_compass(rr)
+    blink_lights(rr, 20)
+    time.sleep(20)
+
+    running = False
+    while running:
+        pass
+
+if __name__ == '__main__':
+    main_loop()
