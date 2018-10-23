6078b9918a46188590950edeefa010df5706833c
Vinzenz Gregor Eck <vinzenz@xal.no>
2018-10-23 12:43:32 +0200

    calibration of compass

--- main.py
+++ main.py
@@ -3,14 +3,35 @@ import sys
 import logging
 from rrb3 import *
 
+import board
+import busio
+import adafruit_bno055
 
 def calibrate_compass(rr):
     """function to calibrate compass"""
     logging.info("calibrating compass")
 
+    i2c = busio.I2C(board.SCL, board.SDA)
+    sensor = adafruit_bno055.BNO055(i2c)
+
+    system = 0
+    gyro = 0
+    accel = 0
+    mag = 0
+
+    sensor.getCalibration(system, gyro, accel, mag)
+    logging.debug("Sys:");
+    logging.debug(system);
+    logging.debug(" G:");
+    logging.debug(gyro);
+    logging.debug(" A:");
+    logging.debug(accel);
+    logging.debug(" M:");
+    logging.debug(mag);
+
     logging.info("calibrating compass .. done")
 
-
+system = gyro = accel = mag = 0;
 def blink_lights(rr, n_blinks, sleep_time=0.5):
     """show blinking lights"""
     logging.info("blinking lights")
